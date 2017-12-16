545871a7c74c679a43e1e099675567311698e559
WeiChien Hsu <weichien711@gmail.com>
2017-12-15 20:34:12 -0600

    :construction: Add HashSet Problem

---
+++ TestFile/src/TestHashSet.java
@@ -0,0 +1,10 @@
+import java.util.HashSet;
+
+public class TestHashSet {
+    public static void main(String[] args) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        hashSet.add(5);
+        System.out.println(hashSet.add(5));
+
+    }
+}
