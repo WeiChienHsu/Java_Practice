9571bbdbca8574365309e590a6e39849ab9f1745
WeiChien Hsu <weichien711@gmail.com>
2017-12-19 18:43:56 -0600

    :construction: Modify fileName

--- 098_Valid_BST_Recurison/main.java
+++ 098_Valid_BST_Recurison/main.java
@@ -9,9 +9,13 @@ private bollean isValidBST(TreeNode root, long max, long min) {
     if (root == null) {
         return true;
     }
+
+    //Current Level : Check root.val
+
     if(root.val >= max || root.val <= min) {
         return false;
     }
 
+    // Return Left and Right
     return isValidBST(root.left, root.val, min) && isValidBST(root.right, max, root.val); 
 }
