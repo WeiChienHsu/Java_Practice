e785ed9541a244a2a188acb06a190babd01d334e
WeiChien Hsu <weichien711@gmail.com>
2017-12-22 21:33:07 -0600

    :bug: Change code

--- 450_Delete_Nodes_in_BST/main.java
+++ 450_Delete_Nodes_in_BST/main.java
@@ -8,16 +8,54 @@
  * }
  */
 class Solution {
-    // Find the height of Left subtree
-    public int height(TreeNode root) {
-        return root == null ? -1 : 1 + height(root.left);
-    }
-    
-    public int countNodes(TreeNode root) {
-        int h = height(root);
-        return h < 0 ? 0 :
-            // check if the left subtree is full with left and right nodes
-            height(root.right) == h-1 ? (1 << h) + countNodes(root.right) // root.right.legt have node
-                                      : (1 << h - 1) + countNodes(root.left);
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null) return root;
+        TreeNode dummy = new TreeNode(Integer.MIN_VALUE);
+        dummy.left = root;
+        TreeNode prev = dummy;
+        TreeNode cur = root;
+            
+            // Find the Node with key
+        while(cur != null && cur.val != key) {
+            prev = cur;
+            if(cur.val < key) {
+                cur = cur.right;
+            } else {
+                cur = cur.left;
+            }
+        }
+        // No found
+        if(cur == null) return dummy.left;
+        
+            // Give a parent Pointer
+        TreeNode target = cur;
+        // Case 1: Two Children
+        if(cur.left != null && cur.right != null) {
+            // Find the leftMost node in right subTree
+            prev = cur;
+            cur = cur.left;
+            while(cur.right != null) {
+                prev = cur;
+                cur = cur.right;
+            }
+            // Change value - 
+            target.val = cur.val;
+        }
+        
+        // Case 2: Delete cur, which has one or no child
+        if(cur.left == null) {
+            if(prev.left == cur) {
+                prev.left = cur.right;
+            } else {
+                prev.right = cur.right;
+            }
+        } else {
+            if(prev.left == cur) {
+                prev.left = cur.left;
+            } else {
+                prev.right = cur.left;
+            }
+        }
+        return dummy.left;
     }
 }
