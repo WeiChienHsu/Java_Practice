f34c906d9bb197a4a1e951f8190202c53508103a
WeiChien Hsu <weichien711@gmail.com>
2017-12-23 01:57:18 -0600

    :construction: Add Permutations_ DFS

--- TestFile/src/permutations.java
+++ TestFile/src/permutations.java
@@ -31,5 +31,45 @@ public class permutations {
         }
         return res;
     }
+
+    public static class permutations2 {
+        public static void main(String[] args) {
+            int[] a = new int[] {1,2,3};
+            System.out.println(permute(a));
+
+        }
+
+        public static List<List<Integer>> permute(int[] nums) {
+            // Corner Case Check
+            List<List<Integer>> res = new ArrayList<List<Integer>>();
+            if(nums == null || nums.length == 0) return res;
+
+            List<Integer> list = new ArrayList<>();
+            helper(nums, list, res, 0);
+            return res;
+        }
+
+        public static void helper(int[] nums, List<Integer> list,List<List<Integer>> res, int pos ){
+            if(pos == nums.length) {
+                res.add(new ArrayList<Integer>(list));
+                return;
+            }
+
+            for(int i = pos; i < nums.length; i++) {
+                list.add(nums[i]);
+                swap(nums, pos, i);
+                helper(nums, list, res, pos + 1);
+                swap(nums, pos, i);
+                list.remove(list.size() - 1);
+            }
+        }
+
+        public static void swap(int[] nums, int pos, int i) {
+            int temp = nums[i];
+            nums[i] = nums[pos];
+            nums[pos] = temp;
+        }
+
+    }
 }
 
