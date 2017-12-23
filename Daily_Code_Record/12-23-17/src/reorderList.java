4cd3aaa6f68a3e213c2963dcf3f75d440374e98e
WeiChien Hsu <weichien711@gmail.com>
2017-12-23 15:53:14 -0600

    :rocket:add

--- Daily_Code_Record/12-23-17/src/reorderList.java
+++ Daily_Code_Record/12-23-17/src/reorderList.java
@@ -1,6 +1,20 @@
 public class reorderList {
 
     public static void main(String[] args) {
+        Node node = new Node<Integer>(1);
+
 
     }
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+        Node(Node<E> prev, E item, Node<E> next) {
+            this.item = item;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
 }
