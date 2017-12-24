ee7b4831f420f28f1a4b6f949b23df4b7d375641
WeiChien Hsu <weichien711@gmail.com>
2017-12-24 16:30:27 -0600

    :racehouse: Finished

---
+++ 653_Two_Sum_II_TreeNode/README.md
@@ -0,0 +1,36 @@
+# Two Sum IV - Input is a BST
+```
+Example 1:
+Input: 
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+
+Target = 9
+
+Output: True
+Example 2:
+Input: 
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+
+Target = 28
+
+Output: False
+```
+
+# Solution - DFS
+- Used a set to safe the number
+- check if there is a target - root.val in the set (If no, add the current root into set)
+- Then check the right and left root, directly return the boolean value.
+```java
+if(root == null) return false;
+        if(set.contains(target - root.val)) return true;
+        set.add(root.val);
+        return helper(root.right, set, target) || helper(root.left, set, target);
+```
---
+++ 653_Two_Sum_II_TreeNode/mian.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        Set<Integer> set = new HashSet<>();
+        return helper(root, set, k);
+    }
+    
+    public boolean helper(TreeNode root, Set set, int target) {
+        if(root == null) return false;
+        if(set.contains(target - root.val)) return true;
+        set.add(root.val);
+        return helper(root.right, set, target) || helper(root.left, set, target);
+        
+        
+    } 
+    
+
+    
+}
