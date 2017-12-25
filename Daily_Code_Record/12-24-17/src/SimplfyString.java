22446fb8242da46acb7598eb06a24752e6a5d248
WeiChien Hsu <weichien711@gmail.com>
2017-12-25 00:03:25 -0600

    :rocket: StringSimplfy

--- Daily_Code_Record/12-24-17/src/SimplfyString.java
+++ Daily_Code_Record/12-24-17/src/SimplfyString.java
@@ -1,32 +1,30 @@
-import java.util.HashMap;
-import java.util.Map;
 
 public class SimplfyString {
-
     public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
-        int count = 0;
-        int[] result = twoSum(nums, target);
-        for(int i = 0; i < result.length; i ++){
-            System.out.println(result[i]);
-        }
+        String s = "/a/./b/../../c/";
 
+        System.out.println(simplifyPath(s));
     }
 
+    public static String simplifyPath(String path){
+        StringBuilder sb = new StringBuilder();
 
-        public static int[] twoSum(int[] nums, int target) {
-            Map<Integer, Integer> map = new HashMap<>();
-            int[] result = new int[2];
+        path = path.trim();
+        String[] strs = path.split("/");
+        int count = 0;
 
-            for(int i = 0; i < nums.length; i++ ) {
-                if(map.containsKey(target - nums[i])) {
-                    result[0] = map.get(target - nums[i]);
-                    result[1] = i;
+        for(int i = strs.length - 1; i >= 0; i-- ) {
+            if(strs[i].equals("..")) {
+                count ++;
+            } else if (!strs[i].equals("") && !strs[i].equals(".")) {
+                if(count > 0) {
+                    count--;
                 } else {
-                    map.put(nums[i], i);
+                    sb.insert(0, "/" + strs[i]);
                 }
             }
-            return result;
         }
+
+        return sb.length() == 0 ? new String("/") : sb.toString();
+    }
 }
