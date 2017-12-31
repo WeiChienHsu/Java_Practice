3f051721ea632a15a50495003d8c9dca96211bd6
WeiChien Hsu <weichien711@gmail.com>
2017-12-30 18:16:45 -0600

    :racehorse: Finished (For both BFS & DFS methods)

--- 102_BinaryTree_Level_Order_Traversal_DFS/main.java
+++ 102_BinaryTree_Level_Order_Traversal_DFS/main.java
@@ -8,26 +8,21 @@
  * }
  */
 class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        
+    public List<List<Integer>> levelOrder(TreeNode root) {        
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         if(root == null) return result;
         
         Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root); 
         
-        queue.add(root); // add the first level
-        
-        // pop out the first level to record in a List and add it's children in the same time
         while (!queue.isEmpty()) {
-            // Record the size for knowing size of next level
             int size = queue.size();
-            // Create a List to record numbers in each level and add in a result List
+
             List<Integer> list = new ArrayList<>();
             
             for(int i = 0; i < size; i++) {
                 TreeNode current = queue.pollFirst();
                 list.add(current.val);
-                // Check if those are null, and add the children nodes of previous root
                 if(current.left != null) queue.add(current.left);
                 if(current.right != null) queue.add(current.right);
             }
