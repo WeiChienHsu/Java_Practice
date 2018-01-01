6531eac1354a5550b594460ef26aff33d747504b
WeiChien Hsu <weichien711@gmail.com>
2017-12-31 20:17:49 -0600

    :rocket:

--- 104_Max_Depth_in_Tree/main.java
+++ 104_Max_Depth_in_Tree/main.java
@@ -1,23 +1,21 @@
-public class Solution {
-    public ListNode detectCycle(ListNode head) {
-        if(head == null) return null;
-        ListNode slow = head;
-        ListNode fast = head;
-        ListNode start = head;
-        
-        while(fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-            if(slow == fast) {
-                // when there is a circle
-                while(slow != start) {
-                    slow = slow.next;
-                    start = start.next;
-                }
-                return start;
-            }  
-        }
-        // There is no circle
-        return null;
+public boolean isValidBST(TreeNode root) {
+    if(root = null) {
+        return true;
     }
+    return isValidBST(root, Long.MAX_VALUE, Long.MIN_VALUE);
+}
+
+private boolean isValidBST(TreeNode root, long max, long min) {
+    if (root == null) {
+        return true;
+    }
+
+    //Current Level : Check root.val
+
+    if(root.val >= max || root.val <= min) {
+        return false;
+    }
+
+    // Return Left and Right
+    return isValidBST(root.left, root.val, min) && isValidBST(root.right, max, root.val); 
 }
