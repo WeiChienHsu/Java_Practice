39cc39a371c96b685e883c373071516c74ede8b3
WeiChien Hsu <weichien711@gmail.com>
2018-02-02 17:35:11 -0600

    :rocker:

---
+++ Daily_Code_Record/02-02-18/src/test.java
@@ -0,0 +1,6 @@
+//public class test {
+//    public static void main(String[] args) {
+//             boolean[][] a = new boolean[1][1];
+//             System.out.println(a[0][0]);
+//        }
+//    }
