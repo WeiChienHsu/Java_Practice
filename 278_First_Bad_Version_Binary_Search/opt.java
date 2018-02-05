603480780af924a50fa0690f67235405fc2d1730
WeiChien Hsu <weichien711@gmail.com>
2018-02-04 23:30:52 -0600

    :rocket: Finished(Binary Search)

---
+++ 278_First_Bad_Version_Binary_Search/opt.java
@@ -0,0 +1,27 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+      public class Solution extends VersionControl {
+        public int firstBadVersion(int n) {
+            int start = 1;
+            int end = n;
+            while(start + 1 < end) {
+                int mid = start + (end - start) / 2;
+                if(isBadVersion(mid)) { //Know if left side still have bad versions
+                    end = mid;
+                } else {
+                    start = mid;
+                }
+            }
+            
+            if(isBadVersion(start)) {
+                return start;
+            }
+            
+            if(isBadVersion(end)) {
+                return end;
+            }
+            
+            return n;   
+        }
+    }
