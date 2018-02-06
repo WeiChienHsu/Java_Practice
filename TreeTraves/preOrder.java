74338157fddc658637d5ad571a17eb93f889a396
WeiChien Hsu <weichien711@gmail.com>
2018-02-06 01:05:07 -0600

    :tree: Preorder

--- TreeTraves/preOrder.java
+++ TreeTraves/preOrder.java
@@ -1,4 +1,4 @@
-
+/* Iteration */
 public List<Integer> preorderTraversal(TreeNode root) {
   ArrayDeque<TreeNode> stack = new ArrayDeque<>();
   List<Integer> preorder = new ArrayList<>();
@@ -26,7 +26,7 @@ public List<Integer> preorderTraversal(TreeNode root) {
 }
 
 // ++++++++++++++++++++++++++++++++++++++++
-
+/* Recursion */
   public List<Integer> preorderTraversal(TreeNode root) {
       List<Integer> preorder = new ArrayList<>();
       traverse(root, preorder);
@@ -41,3 +41,25 @@ public List<Integer> preorderTraversal(TreeNode root) {
       traverse(root.left, preorder);
       traverse(root.right, preorder);
   }
+
+// +++++++++++++++++++++++++++++++++++++++++
+/* Divide and Conquer */
+
+public List<Integer> preorderTraversal(TreeNode root) {
+  ArrayList<Integer> res = new ArrayList<>();
+  
+  if(root == null) {
+      return res;
+  }
+  
+  // Divide
+  List<Integer> left = preorderTraversal(root.left); //[2, 4, 5]
+  List<Integer> right = preorderTraversal(root.right); //[3]
+  
+  // Conquer (Merge)
+  res.add(root.val);
+  res.addAll(left);
+  res.addAll(right);
+  
+  return res; // [1] + [2,4,5] + [3]
+}
