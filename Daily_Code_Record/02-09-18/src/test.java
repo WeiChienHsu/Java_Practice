eb03a807a470446a9991f85eea51bb66c6cd77c8
WeiChien Hsu <weichien711@gmail.com>
2018-02-09 12:14:21 -0600

    :rocket: Static Method/Class/Variable

---
+++ Daily_Code_Record/02-09-18/src/test.java
@@ -0,0 +1,19 @@
+public class test {
+    public static void main(String[] args){
+        Duck a = new Duck(33);
+        Duck b = new Duck(23);
+        Duck c = new Duck(43);
+        Duck d = new Duck(43);
+        Duck e = new Duck(43);
+        Duck f = new Duck(43);
+        Duck g = new Duck(43);
+        System.out.println(a.getSize());
+        System.out.println(a.getDuckCount());
+        System.out.println(b.getSize());
+        System.out.println(b.getDuckCount());
+        System.out.println(c.getSize());
+        System.out.println(c.getDuckCount());
+
+    }
+
+}
