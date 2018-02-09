ffed9e57ae4a17a96621f49eae547b02ea21100b
WeiChien Hsu <weichien711@gmail.com>
2018-02-08 22:58:23 -0600

    :rocket: ResultType_divdiNconquer

--- 110_Balanced_Binary_Tree/DFS.java
+++ 110_Balanced_Binary_Tree/DFS.java
@@ -15,3 +15,24 @@ class Solution {
       return Math.max(left, right) + 1;
   }
 }
+
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return maxDepth(root) != -1;
+    }
+    
+    private int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        if(left == -1 || right == -1 || Math.abs(left - right) > 1) {
+            return -1;
+        }
+        
+        return Math.max(right, left) + 1;
+    }
+}
