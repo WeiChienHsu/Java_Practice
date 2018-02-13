2a9edfc3d850e42d77851138868e0226e40bebd2
WeiChien Hsu <weichien711@gmail.com>
2018-02-12 20:41:02 -0600

    :rocket: Send From SF :tada:

---
+++ Daily_Code_Record/02-10-18/src/BinarySearch.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinarySearch {
+    List<Integer> a = new ArrayList<>();
+    int start = 0;
+    int end = a.size();
+
+    while(start + 1 < end){
+        int mid = start + (end - start)/2 ;
+        if(num[s])
+    }
+}
