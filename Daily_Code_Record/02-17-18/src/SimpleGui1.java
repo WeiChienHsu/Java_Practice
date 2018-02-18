880c18c5b0dfcc4b3a86817d21781c4573186a21
WeiChien Hsu <weichien711@gmail.com>
2018-02-17 22:29:04 -0600

    Java GUI

--- Daily_Code_Record/02-17-18/src/SimpleGui1.java
+++ Daily_Code_Record/02-17-18/src/SimpleGui1.java
@@ -4,6 +4,7 @@ import java.awt.event.*;
 public class SimpleGui1 implements ActionListener {
     JButton button;
 
+
     public static void main(String[] args) {
         SimpleGui1 gui = new SimpleGui1();
         gui.go();
@@ -13,9 +14,11 @@ public class SimpleGui1 implements ActionListener {
         JFrame frame = new JFrame();
         button = new JButton("Click Me");
 
+
         button.addActionListener(this);
 
         frame.getContentPane().add(button);
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 300);
         frame.setVisible(true);
