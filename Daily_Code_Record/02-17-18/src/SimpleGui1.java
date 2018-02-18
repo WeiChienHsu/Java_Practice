6904be92e435fe189e8dad3825039ffb85dd0054
WeiChien Hsu <weichien711@gmail.com>
2018-02-17 21:57:46 -0600

    :rocket: EventListener / EventSource / Interface

---
+++ Daily_Code_Record/02-17-18/src/SimpleGui1.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class SimpleGui1 implements ActionListener {
+    JButton button;
+
+    public static void main(String[] args) {
+        SimpleGui1 gui = new SimpleGui1();
+        gui.go();
+    }
+
+    public void go() {
+        JFrame frame = new JFrame();
+        button = new JButton("Click Me");
+
+        button.addActionListener(this);
+
+        frame.getContentPane().add(button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+    public void actionPerformed(ActionEvent e){
+        button.setText("I've been checked!");
+    }
+}
