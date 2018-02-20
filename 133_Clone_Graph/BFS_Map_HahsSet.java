7b2a6bc2d233fbee755b8c2cd3bba21ddbb29159
WeiChien Hsu <weichien711@gmail.com>
2018-02-19 21:28:10 -0600

    :rocket: BFS: 1. find all nodes(Queue+Set) 2. Copy nodes(Map) 3. Copy Edges(Map) 4. return map.get(node)

---
+++ 133_Clone_Graph/BFS_Map_HahsSet.java
@@ -0,0 +1,43 @@
+public class Solution {
+  public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+      if(node == null) {
+          return node;
+      }
+      // Use bfs to traverse the graph and get all nodes
+      ArrayList<UndirectedGraphNode> nodes = getNodes(node);
+      
+      // copy nodes, store the old -> new mapping information in a hash map
+      HashMap<UndirectedGraphNode, UndirectedGraphNode> mapping = new HashMap<>();
+      for(UndirectedGraphNode n : nodes) {
+          mapping.put(n, new UndirectedGraphNode(n.label));
+      }
+      
+      // copy neighbors(edges)
+      for(UndirectedGraphNode n : nodes) {
+          UndirectedGraphNode newNode = mapping.get(n);
+          for(UndirectedGraphNode neighbor : n.neighbors) {
+              UndirectedGraphNode newNeighbor = mapping.get(neighbor);
+              newNode.neighbors.add(newNeighbor);
+          }
+      }
+      return mapping.get(node);
+  }
+  
+  private ArrayList<UndirectedGraphNode> getNodes(UndirectedGraphNode node) {
+      Deque<UndirectedGraphNode> queue = new ArrayDeque<UndirectedGraphNode>();
+      HashSet<UndirectedGraphNode> set = new HashSet<>();
+      
+      queue.offer(node);
+      set.add(node);
+      while(!queue.isEmpty()){
+          UndirectedGraphNode head = queue.pollFirst();
+          for(UndirectedGraphNode neighbor : head.neighbors) {
+              if(!set.contains(neighbor)) {
+                  set.add(neighbor);
+                  queue.offerLast(neighbor);
+              }
+          }
+      }
+      return new ArrayList<UndirectedGraphNode>(set);
+  }
+}
