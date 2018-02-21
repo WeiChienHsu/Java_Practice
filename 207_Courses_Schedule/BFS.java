bb5f60dcad74f7831aca56a38c374ffbbf276fa1
WeiChien Hsu <weichien711@gmail.com>
2018-02-20 19:07:17 -0600

    :rocket: BFS- CountIndegree + Find StartNodes + Sort the Order

--- 207_Courses_Schedule/BFS.java
+++ 207_Courses_Schedule/BFS.java
@@ -1,41 +1,43 @@
 class Solution {
-  public boolean canFinish(int numCourses, int[][] prerequisites) {
-      ArrayList[] graph = new ArrayList[numCourses];
-      int[] degree = new int[numCourses];
-      Deque<Integer> queue = new ArrayDeque<>();
-      int count = 0;
-      
-      // Init the Graph
-      for(int i = 0; i < numCourses; i++) {
-          graph[i] = new ArrayList();
-      }
-      
-      // Init the Level
-      for(int i = 0; i < prerequisites.length; i++){
-          degree[prerequisites[i][0]]++;;
-          graph[prerequisites[i][1]].add(prerequisites[i][0]);
-      }
-      
-      
-      for(int i = 0; i < degree.length; i++) {
-          if(degree[i] == 0) {
-              queue.offerLast(i);
-              count++;
-          }
-      }
-      
-      while(!queue.isEmpty()) {
-          int course = queue.pollFirst();
-          for(int i = 0; i < graph[course].size(); i++) {
-              int pointer = (int) graph[course].get(i);
-              degree[pointer]--;
-              if(degree[pointer] == 0) {
-                  queue.offerLast(pointer);
-                  count++;
-              }
-          }
-      }
-      
-      return count == numCourses? true : false;
-  }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList[] graph = new ArrayList[numCourses];
+        int[] indegree = new int[numCourses];
+        Deque<Integer> queue = new ArrayDeque<>();
+        int count = 0;
+        
+        // Init graph
+        for(int i = 0; i < numCourses; i++) {
+            graph[i] = new ArrayList<Integer>();
+        }
+        
+        // Add neighbor into ArrayList
+        for(int i = 0; i < prerequisites.length; i++) {
+            indegree[prerequisites[i][0]]++;
+            graph[prerequisites[i][1]].add(prerequisites[i][0]);
+        }
+        
+        // Find StartNodes
+        for(int i = 0; i < indegree.length; i++){
+            if(indegree[i] == 0){
+                queue.add(i);
+                count++;
+            }
+        }
+        
+        while(!queue.isEmpty()) {
+            int course = queue.pollFirst();
+            for(int i = 0; i < graph[course].size(); i++) {
+                int neighbor = (int)graph[course].get(i);
+                indegree[neighbor]--;
+                if(indegree[neighbor] == 0){
+                    queue.offerLast(neighbor);
+                    count++;
+                }
+            }
+        }
+        
+        return count == numCourses;
+    }
+    
+
 }
