93ab4a0a13826f4c373b67a8aff9d7ff1d114fa9
WeiChien Hsu <weichien711@gmail.com>
2018-06-07 23:51:19 -0500

    :constrction: Tree (Preoreder)

---
+++ 606_Construct_StringByTree/Tree.java
@@ -0,0 +1,20 @@
+public String tree2str(TreeNode t) {
+  StringBuilder sb = new StringBuilder();
+  helper(sb,t);
+  return sb.toString();
+}
+public void helper(StringBuilder sb,TreeNode t){
+  if(t!=null){
+      sb.append(t.val);
+      if(t.left!=null||t.right!=null){
+          sb.append("(");
+          helper(sb,t.left);
+          sb.append(")");
+          if(t.right!=null){
+              sb.append("(");
+          helper(sb,t.right);
+          sb.append(")");
+          }
+      }
+  }
+}
