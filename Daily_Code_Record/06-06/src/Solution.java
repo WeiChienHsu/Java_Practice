3e0f1e1af45d27d1988501cb6e63ceb109210e5a
WeiChien Hsu <weichien711@gmail.com>
2018-06-10 21:55:34 -0500

    :rocket: Create new Function

--- Daily_Code_Record/06-06/src/Solution.java
+++ Daily_Code_Record/06-06/src/Solution.java
@@ -1,11 +1,51 @@
 public class Solution {
     public static void main(String[] args) {
-        int[] result = new int[]{2 ,2};
-        System.out.println(printResult(result));
+//        Set<Integer> set = new HashSet<>(Arrays.asList(2, 3, 5, 7, 11, 13));
+//        System.out.println(set.contains(4));
 
+        String newString = "apple";
+        System.out.println(newString.indexOf("ab"));
+    }
+
+    public static int getPrime(int num) {
+        int count = 0;
+        int currentNumber = 2;
+        while(count < num) {
+            if(isPrime(currentNumber)) {
+                System.out.print(currentNumber);
+                System.out.print(" ");
+                count++;
+            }
+            currentNumber++;
+        }
+        return count;
+    }
+
+    public static boolean isPrime(int num) {
+        if(num == 1) return false;
+        for(int i = 2; i < num; i++) {
+            if(num % i == 0) return false;
+        }
+        return true;
     }
 
     public static int printResult(int[] result) {
         return result[0];
+    }
+
+    public static int countTwoPowerNumber(int num) {
+        int ten = 1;
+        for(int i = 0; i < num; i++) {
+            ten *= 10;
+        }
+
+        int count = 0;
+        int two = 1;
+        while(two < ten) {
+            two *= 2;
+            count++;
+        }
+        System.out.println(two);
+        return count;
     }
 }
