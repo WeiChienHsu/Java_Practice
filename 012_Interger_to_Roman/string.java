7646fb9fda1d35293fee192fd45fbeaa174446a8
WeiChien Hsu <weichien711@gmail.com>
2018-06-21 11:47:49 -0500

    :construction: string

---
+++ 012_Interger_to_Roman/string.java
@@ -0,0 +1,15 @@
+class Solution {
+  public String intToRoman(int num) {
+      int[] values = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+      String[] signs = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+      StringBuilder sb = new StringBuilder();
+      
+      for(int i = 0; i < values.length; i++) {
+          while(num >= values[i]) {
+              num -= values[i];
+              sb.append(signs[i]);
+          }
+      }
+      return sb.toString();
+  }
+}
