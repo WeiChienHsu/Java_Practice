28c9ff9efa4c82799c74abecbb6660dafba37ed2
WeiChien Hsu <weichien711@gmail.com>
2018-09-01 14:40:08 -0500

    :construction:

---
+++ 771_Jewels_and_Stone/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+  public int numJewelsInStones(String J, String S) {
+      Set<Character> jewels = new HashSet<>();
+      int count = 0;
+      
+      for(int i = 0; i < J.length(); i++) {
+          jewels.add(J.charAt(i));
+      }
+      
+      for(int i = 0; i < S.length(); i++) {
+          if(jewels.contains(S.charAt(i))) count ++;
+      }
+      return count;
+  }
+}
