055cd92bb97633839c160020b96c8fc5d835f4ca
WeiChien Hsu <weichien711@gmail.com>
2018-09-01 22:27:05 -0500

    :rocket: Opt the solution

--- 844_Backspace_String_Compare/StringBuilder.java
+++ 844_Backspace_String_Compare/StringBuilder.java
@@ -1,35 +1,24 @@
 class Solution {
-  public boolean backspaceCompare(String S, String T) {
-      StringBuilder sb1 = new StringBuilder();
-      StringBuilder sb2 = new StringBuilder();
-      
-      
-      for(int i = 0; i < S.length(); i++) {
-          if(S.charAt(i) == '#') {
-              if(sb1.length() != 0) {
-                  sb1.deleteCharAt(sb1.length() - 1);
-              }
-              continue;
-          }
-          
-          sb1.append(S.charAt(i));
-      }
-      
-      String newS = String.valueOf(sb1);
-      
-      for(int i = 0; i < T.length(); i++) {
-          if(T.charAt(i) == '#') {
-              if(sb2.length() != 0) {
-                  sb2.deleteCharAt(sb2.length() - 1);
-              }
-              continue;
-          }
-          
-          sb2.append(T.charAt(i));
-      }
-      
-      String newT = String.valueOf(sb2);
-      
-      return newS.equals(newT);
-  }
+    public boolean backspaceCompare(String S, String T) {
+        StringBuilder sb_s = new StringBuilder();
+        StringBuilder sb_t = new StringBuilder();
+        
+        for(int i = 0; i < S.length(); i++) {
+            if(S.charAt(i) == '#' && sb_s.length() > 0) {
+                sb_s.deleteCharAt(sb_s.length() - 1);
+            } else if(S.charAt(i) != '#') {
+                sb_s.append(S.charAt(i));
+            }
+        }
+        
+        for(int i = 0; i < T.length(); i++) {
+            if(T.charAt(i) == '#' && sb_t.length() > 0) {
+                sb_t.deleteCharAt(sb_t.length() - 1);
+            } else if(T.charAt(i) != '#') {
+                sb_t.append(T.charAt(i));
+            }
+        }
+        
+        return Objects.equals(sb_t.toString(), sb_s.toString());
+    }
 }
