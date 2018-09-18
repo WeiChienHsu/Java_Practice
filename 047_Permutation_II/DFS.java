a53d6ba367b1bb21679f87aa4de73178370711d1
WeiChien Hsu <weichien711@gmail.com>
2018-09-18 15:36:05 -0500

    :bug: Fix the Solution about Permutation

--- 047_Permutation_II/DFS.java
+++ 047_Permutation_II/DFS.java
@@ -1,36 +1,45 @@
+/*
+    Sorted the array to make sure the situation likes [3,3,0,3] which might not deal with the last 3 in my method
+
+    Maintains only one variables:
+        - tempList: record the current add-in result
+        - boolean visited[]: record the index of number that has been added
+        
+    Only one step to do:
+        - Add in a new element after checking its not exist in the list
+    
+    Base case:
+        - If the length of temp list is equal the origin arr, add them into the result list
+
+*/
+
+
 class Solution {
-  public List<List<Integer>> permuteUnique(int[] nums) {
-      List<List<Integer>> results = new ArrayList<>();
-      if(nums == null || nums.length == 0) {
-          return results;
-      }
-      List<Integer> permutation = new ArrayList<>();
-      boolean[] visited = new boolean[nums.length];
-      dfsHelper(results, permutation, visited, nums);
-      return results;
-  }
-  
-  public void dfsHelper(List<List<Integer>> results, List<Integer> permutation, boolean[] visited, int[] nums){
-      
-      if(permutation.size() == nums.length){
-          results.add(new ArrayList<>(permutation));
-          return;
-      }
-      
-      for(int i = 0; i < nums.length; i++) {
-          if(visited[i]){
-              continue;
-          }
-          
-          if(i != 0 && nums[i] == nums[i - 1] && !visited[i-1]){
-              continue;
-          }
-          
-          permutation.add(nums[i]);
-          visited[i] = !visited[i];
-          dfsHelper(results, permutation, visited, nums);
-          visited[i] = !visited[i];
-          permutation.remove(permutation.size() - 1);
-      }
-  }
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
+        Arrays.sort(nums);
+        dfsHelper(results, nums, visited, new ArrayList<>());
+        return results;
+    }
+    
+    public void dfsHelper(List<List<Integer>> results, int[] nums, boolean[] visited, List<Integer> tempList) {
+        
+        if(tempList.size() == nums.length) {
+            results.add(new ArrayList<>(tempList));
+            return;
+        }
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(visited[i]) continue;
+            
+            if(i != 0 && nums[i] == nums[i - 1] && !visited[i - 1]) continue;
+            
+            tempList.add(nums[i]);
+            visited[i] = !visited[i];
+            dfsHelper(results, nums, visited, tempList);
+            visited[i] = !visited[i];
+            tempList.remove(tempList.size() - 1);
+        }
+    }
 }
